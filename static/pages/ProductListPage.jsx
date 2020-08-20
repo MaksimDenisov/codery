9d145d09a040c39a472618133a2cff2b9ce312aa
max <max.nick.demax@gmail.com>
2020-08-20 12:11:20 +0300

    solve 7.7

--- static/pages/ProductListPage.jsx
+++ static/pages/ProductListPage.jsx
@@ -6,36 +6,46 @@ import {Link} from "react-router-dom";
  */
 export default class ProductListPage extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+        fetch("/api/products")
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (json) {
+                this.setState({products: json});
+            }.bind(this));
+    }
+
     render() {
         return <React.Fragment>
             <main>
                 <div className="row mt-3">
-                    <div className="col-5 col-sm-4 mt-3 offset-1 offset-sm-2">
-                        <div className="row">
-                            <h3>ПВУ Turkov ZENIT 350 HECO</h3>
-                            <img className="col-6 offset-3 img-fluid" src="public/product1.png"/>
-                        </div>
-                        <div className="row mt-3 mb-3">
-                            <div className="col-3 offset-3  pl-0">
-                                <Link className="btn btn-primary font-weight-bold" to="/products/1">Заказать</Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-5 col-sm-4 mt-3">
-                        <div className="row">
-                            <h3>ПВУ Turkov ZENIT 350 HECO ИСП.2</h3>
-                            <img className="col-6 offset-3  img-fluid" src="public/product2.png"/>
-                        </div>
-                        <div className="row mt-3 mb-3">
-                            <div className="col-3 offset-3  pl-0">
-                                <Link to="/products/2">
-                                    <button className="btn btn-primary font-weight-bold">Заказать</button>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
+                    {
+                        this.state.products && this.renderProducts()
+                    }
                 </div>
             </main>
         </React.Fragment>;
+    }
+
+    renderProducts() {
+        return this.state.products.map((item, index) => {
+            return <div className={'col-5 col-sm-4 mt-3' + ((index % 2 == 0) ? ' offset-1 offset-sm-2' : '')}>
+                <div className="row">
+                    <h3>{item.description}</h3>
+                    <img className="col-6 offset-3 img-fluid" src={'/public/' + item.img}/>
+                </div>
+                <div className="row mt-3 mb-3">
+                    <div className="col-3 offset-3  pl-0">
+                        <Link className="btn btn-primary font-weight-bold" to="/products/1">Заказать</Link>
+                    </div>
+                </div>
+            </div>;
+        });
     }
 }
